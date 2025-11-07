519f0a36aee390a083f62c5ed7998953f00dbfa5
charlesnoah344 <23397@ecam.be>
2025-11-07 11:56:18 +0100

    Merge complete_nav. Les 4 boutons de navigations fonctionnels

---
+++ ViewModels/BonusPageViewModel.cs
@@ -0,0 +1,6 @@
+namespace Chess_D_B.ViewModels;
+
+public partial class BonusPageViewModel : ViewModelBase
+{
+    public string Test { get; set; } = "Bonus";
+}
---
+++ ViewModels/EloPageViewModel.cs
@@ -0,0 +1,6 @@
+namespace Chess_D_B.ViewModels;
+
+public partial class EloPageViewModel : ViewModelBase
+{
+    public string Test { get; set; } = "ELO";
+}
