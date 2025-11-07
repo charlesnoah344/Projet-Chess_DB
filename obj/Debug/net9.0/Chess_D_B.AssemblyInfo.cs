fbb05d2d785e030a5153aeedc9a81e044d2d3585
charlesnoah344 <23397@ecam.be>
2025-11-07 20:57:52 +0100

    commit avant merge de loic_model

--- obj/Debug/net9.0/Chess_D_B.AssemblyInfo.cs
+++ obj/Debug/net9.0/Chess_D_B.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chess_D_B")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb341917cd8f0cf765355cc3d1127186df77927")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519f0a36aee390a083f62c5ed7998953f00dbfa5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chess_D_B")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chess_D_B")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
